4f4c864e37ede7943b1b085fd48dc5be9f19b93d
Lasse Lysskov <113604609+LasseLysskov@users.noreply.github.com>
2023-03-31 12:43:47 +0200

    Initial commit

---
+++ src/main/java/com/example/cookiedemoproject/CookieDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cookiedemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookieDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cookiedemoproject/CookieDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cookiedemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
